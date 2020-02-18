d8bfe5d59a92e40df669412e0c2b7b070a410314
yaochenzhi <yaochenzhi@localhost.localdomain>
2020-02-18 21:11:02 +0800

    copy

--- src/main/java/com/yaochenzhi/hdfs/HDFSClient.java
+++ src/main/java/com/yaochenzhi/hdfs/HDFSClient.java
@@ -7,6 +7,8 @@ import java.net.URISyntaxException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsAction;
+import org.junit.jupiter.api.Test;
 
 public class HDFSClient {
 	public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException{
@@ -24,4 +26,14 @@ public class HDFSClient {
 		System.out.println("Well done!");
 		
 	}
+	
+	@Test
+	public void testCopyFromLocalFile() throws IOException, InterruptedException, URISyntaxException{
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(new URI("hdfs://192.168.1.11:9000"), conf, "hadoop");
+		fs.copyFromLocalFile(new Path("d:/readme.txt"), new Path("/com.yaochenzhi.hdfs/readme.txt"));
+		fs.close();
+		System.out.println("Well done 2!");
+	}
+	
 }
